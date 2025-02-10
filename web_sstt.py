18fe022357a745a431d7e4ffec7919ac7fb1c9dd
J0S3-ANT0NI0 <160168754+J0S3-ANT0NI0@users.noreply.github.com>
2025-02-10 19:06:03 +0100

    Update web_sstt.py

--- web_sstt.py
+++ web_sstt.py
@@ -18,6 +18,8 @@ import logging      # Para imprimir logs
 BUFSIZE = 8192 # Tamaño máximo del buffer que se puede utilizar
 TIMEOUT_CONNECTION = 20 # Timout para la conexión persistente
 MAX_ACCESOS = 10
+BACKLOG = 64 
+AUMENTO_COOKIE_POR_DEFECTO = 1
 
 # Extensiones admitidas (extension, name in HTTP)
 filetypes = {"gif":"image/gif", "jpg":"image/jpg", "jpeg":"image/jpeg", "png":"image/png", "htm":"text/htm", 
@@ -70,11 +72,20 @@ def process_cookies(headers,  cs):
                 found = True
                 return MAX_ACCESOS
             else:
-                i='Cookie: '+ i.split(":")[1] + 1
+                """i='Cookie: '+ i.split(":")[1] + 1
+                found = True
+                cadena = ' '.join(cabeceras)
+                cadena = cadena.encode()
+                cs.send(cadena)"""
+                cookie_valor = i.split(":")[1]
+                cookie_int = int(cookie_valor)
+                cookie_int=+ AUMENTO_COOKIE_POR_DEFECTO
+                i.split(":")[1] = cookie_int
                 found = True
                 cadena = ' '.join(cabeceras)
                 cadena = cadena.encode()
                 cs.send(cadena)
+                return cookie_int
     if not found:
         cabeceras.append('Cookie: 1\n')
         cadena = ' '.join(cabeceras)
@@ -83,42 +94,68 @@ def process_cookies(headers,  cs):
         return 1
 
 
+
 def process_web_request(cs, webroot):
     """ Procesamiento principal de los mensajes recibidos.
         Típicamente se seguirá un procedimiento similar al siguiente (aunque el alumno puede modificarlo si lo desea)
 
-        * Bucle para esperar hasta que lleguen datos en la red a través del socket cs con select()
+        * Bucle para esperar hasta que lleguen datos en la red a través del socket cs con select()"""
+    while True:
 
-            * Se comprueba si hay que cerrar la conexión por exceder TIMEOUT_CONNECTION segundos
-              sin recibir ningún mensaje o hay datos. Se utiliza select.select
+        """ * Se comprueba si hay que cerrar la conexión por exceder TIMEOUT_CONNECTION segundos
+              sin recibir ningún mensaje o hay datos. Se utiliza select.select """
+        rsublist, _, _ = select.select([cs],[],[],TIMEOUT_CONNECTION)
+        ## * Si no es por timeout y hay datos en el socket cs
+        if cs in rsublist:
+         ##       * Leer los datos con recv.
+            datos = cs.recv(BUFSIZE)
+            if len(datos) <= 0:
+                break
+            datos = datos.decode()
+            logger.info(datos)
+         ##       * Analizar que la línea de solicitud y comprobar está bien formateada según HTTP 1.1
+            
+         ##          * Devuelve una lista con los atributos de las cabeceras.
 
-            * Si no es por timeout y hay datos en el socket cs.
-                * Leer los datos con recv.
-                * Analizar que la línea de solicitud y comprobar está bien formateada según HTTP 1.1
-                    * Devuelve una lista con los atributos de las cabeceras.
-                    * Comprobar si la versión de HTTP es 1.1
-                    * Comprobar si es un método GET o POST. Si no devolver un error Error 405 "Method Not Allowed".
-                    * Leer URL y eliminar parámetros si los hubiera
-                    * Comprobar si el recurso solicitado es /, En ese caso el recurso es index.html
-                    * Construir la ruta absoluta del recurso (webroot + recurso solicitado)
-                    * Comprobar que el recurso (fichero) existe, si no devolver Error 404 "Not found"
-                    * Analizar las cabeceras. Imprimir cada cabecera y su valor. Si la cabecera es Cookie comprobar
-                      el valor de cookie_counter para ver si ha llegado a MAX_ACCESOS.
-                      Si se ha llegado a MAX_ACCESOS devolver un Error "403 Forbidden"
-                    * Obtener el tamaño del recurso en bytes.
-                    * Extraer extensión para obtener el tipo de archivo. Necesario para la cabecera Content-Type
-                    * Preparar respuesta con código 200. Construir una respuesta que incluya: la línea de respuesta y
-                      las cabeceras Date, Server, Connection, Set-Cookie (para la cookie cookie_counter),
-                      Content-Length y Content-Type.
-                    * Leer y enviar el contenido del fichero a retornar en el cuerpo de la respuesta.
-                    * Se abre el fichero en modo lectura y modo binario
-                        * Se lee el fichero en bloques de BUFSIZE bytes (8KB)
-                        * Cuando ya no hay más información para leer, se corta el bucle
+         ##           * Comprobar si la versión de HTTP es 1.1
 
-            * Si es por timeout, se cierra el socket tras el período de persistencia.
+         ##           * Comprobar si es un método GET o POST. Si no devolver un error Error 405 "Method Not Allowed".
+
+         ##           * Leer URL y eliminar parámetros si los hubiera
+
+         ##           * Comprobar si el recurso solicitado es /, En ese caso el recurso es index.html
+
+         ##           * Construir la ruta absoluta del recurso (webroot + recurso solicitado)
+
+         ##          * Comprobar que el recurso (fichero) existe, si no devolver Error 404 "Not found"
+
+         ##           * Analizar las cabeceras. Imprimir cada cabecera y su valor. Si la cabecera es Cookie comprobar
+                        #process_cookies(value,cs)
+         ##             el valor de cookie_counter para ver si ha llegado a MAX_ACCESOS.
+         
+         ##             Si se ha llegado a MAX_ACCESOS devolver un Error "403 Forbidden"
+         
+         ##           * Obtener el tamaño del recurso en bytes.
+         
+         ##           * Extraer extensión para obtener el tipo de archivo. Necesario para la cabecera Content-Type
+         
+         ##           * Preparar respuesta con código 200. Construir una respuesta que incluya: la línea de respuesta y
+         
+         ##             las cabeceras Date, Server, Connection, Set-Cookie (para la cookie cookie_counter),
+         
+         ##             Content-Length y Content-Type.
+         
+         ##           * Leer y enviar el contenido del fichero a retornar en el cuerpo de la respuesta.
+         
+         ##           * Se abre el fichero en modo lectura y modo binario
+        
+        ##              * Se lee el fichero en bloques de BUFSIZE bytes (8KB)
+        
+        ##                * Cuando ya no hay más información para leer, se corta el bucle
+        
+        """    * Si es por timeout, se cierra el socket tras el período de persistencia.
                 * NOTA: Si hay algún error, enviar una respuesta de error con una pequeña página HTML que informe del error.
-    """
-
+        """
 
 def main():
     """ Función principal del servidor
@@ -144,36 +181,27 @@ def main():
 
 
 
-        thing = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
-        thing.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        thing.bind((args.host, args.port))
-        thing.listen()
+        servidor = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
+        servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        servidor.bind((args.host, args.port))
+        servidor.listen(BACKLOG)
         while True:
-            conn, addr = thing.accept()
-            values = recibir_mensaje(conn)
-            process_cookies(values, conn)
-            #enviar_mensaje(conn,values)
-            logger.info('Cliente {} connected.'.format(addr))
-            conn.close()
+            cliente_socket, cliente_direccion = servidor.accept()
+         ## - Creamos un proceso hijo
+            pid = os.fork()
 
-            """ Funcionalidad a realizar
-            * Crea un socket TCP (SOCK_STREAM)
-            * Permite reusar la misma dirección previamente vinculada a otro proceso. Debe ir antes de sock.bind
-            * Vinculamos el socket a una IP y puerto elegidos
-    
-            * Escucha conexiones entrantes
-    
-            * Bucle infinito para mantener el servidor activo indefinidamente
-                - Aceptamos la conexión
-    
-                - Creamos un proceso hijo
-    
-                - Si es el proceso hijo se cierra el socket del padre y procesar la petición con process_web_request()
-    
-                - Si es el proceso padre cerrar el socket que gestiona el hijo.
-            """
+            ## - Si es el proceso hijo se cierra el socket del padre y procesar la petición con process_web_request()
+            if pid == 0:  
+                servidor.close()
+                process_web_request(cliente_socket, args.webroot) 
+                ## values = recibir_mensaje(cliente_socket)          
+                ##process_cookies(values, cliente_socket)
+                
+            ## - Si es el proceso padre cerrar el socket que gestiona el hijo.
+            else:  # Proceso padre
+                cliente_socket.close()  # El padre cierra la conexión con el cliente
     except KeyboardInterrupt:
-        cerrar_conexion(thing)
+        cerrar_conexion(servidor)
 
 if __name__== "__main__":
     main()
